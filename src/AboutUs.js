58b71a492ae58acf4e31c31e4c807af3deabcc04
ErzhenaDorzhaeva <erzhena-sempai@mail.ru>
2023-07-26 21:28:23 +0800

    photomove

---
+++ src/AboutUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutUs() {
+  return <h2> Мы крутые! </h2>;
+}
+
+export default AboutUs;
---
+++ src/Information.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Information() {
+  return <h2> Информация </h2>;
+}
+
+export default Information;
